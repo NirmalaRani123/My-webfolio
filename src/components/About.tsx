82d4657eb040efef42aa4fc0aa3ef1a05f259595
NirmalaRani123 <162834067+NirmalaRani123@users.noreply.github.com>
2024-11-11 17:21:19 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id='about' className='container pt-32'>
-        <h2 className='text-4xl md:text-5xl' data-aos="zoom-out-up">About Me</h2>
-        <p className='text-black-500 pt-4' data-aos="zoom-out-up">
-        I'm currently a student at GIAIC, where I'm pursuing an advanced course in Artificial Intelligence, Web 3.0, and the Metaverse. I have a deep passion for technology and am committed to continually expanding my skill set to stay current with the latest advancements in the field.
+    <div id="about" className="container pt-32">
+        <h2 className="text-4xl md:text-5xl" data-aos="zoom-out-up">About Me</h2>
+        <p className="text-black-500 pt-4" data-aos="zoom-out-up">
+        I am currently a student at GIAIC, where I am pursuing an advanced course in Artificial Intelligence, Web 3.0, and the Metaverse. I have a deep passion for technology and am committed to continually expanding my skill set to stay current with the latest advancements in the field.
         </p>
       
     </div>
