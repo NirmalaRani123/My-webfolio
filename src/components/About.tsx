05ecc19fe662bec37e14adbf88d157e0a6c645ea
NirmalaRani123 <162834067+NirmalaRani123@users.noreply.github.com>
2024-11-11 17:25:08 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,9 +5,8 @@ const About = () => {
     <div id="about" className="container pt-32">
         <h2 className="text-4xl md:text-5xl" data-aos="zoom-out-up">About Me</h2>
         <p className="text-black-500 pt-4" data-aos="zoom-out-up">
-        I am currently a student at GIAIC, where I am pursuing an advanced course in Artificial Intelligence, Web 3.0, and the Metaverse. I have a deep passion for technology and am committed to continually expanding my skill set to stay current with the latest advancements in the field.
+          I am currently a student at GIAIC, where I am pursuing an advanced course in Artificial Intelligence, Web 3.0, and the Metaverse. I have a deep passion for technology and am committed to continually expanding my skill set to stay current with the latest advancements in the field.
         </p>
-      
     </div>
   )
 }
